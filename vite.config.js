2043f9aa33894e73a4c1f0eb7a919c4a90b9e8d0
Your Name <email@example.com>
2023-06-13 00:39:35 +0800

    test 1

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mall/",
+  base: "/mall.github.io/",
   build: {
     outDir: "docs"
   },
